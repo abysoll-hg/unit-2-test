cb97161852ddf5753e5bead353ccaac980c833bd
abysoll-hg <slickspam4u@gmail.com>
2025-12-15 09:11:47 -0800

    Update KillCilantro.java

--- KillCilantro.java
+++ KillCilantro.java
@@ -8,7 +8,18 @@ public class KillCilantro {
       }
       return returnStr;
    }
+   public static String killWord(String sentence, String word, String replacement) {
+      String returnStr = sentence;
+      int f = returnStr.indexOf(word);
+      while (f != -1) {
+          returnStr = returnStr.substring(0,f) + replacement + returnStr.substring(f+(replacement.length()+Math.abs(replacement.length()-word.length())));
+          f = returnStr.indexOf(word);
+      }
+      return returnStr;
+   }
    public static void main(String[] Args) {
-      System.out.println(killCilantro("cilantro is very good cilantro cilantro"));
+      System.out.println(killWord("cilantro is very good cilantro cilantro","cilantro","fdsasadf"));
+      System.out.println(killCilantro("cilantrois very good cilantro cilantro"));
+
    }
 }
